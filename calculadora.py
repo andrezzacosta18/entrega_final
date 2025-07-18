30c876e4e0ccf7f7a5fced6678dd676c202497d4
andrezzacosta18 <andrezzacosta18@gmail.com>
2025-07-18 10:07:16 -0300

    Adicionar calculadora python

---
+++ calculadora.py
@@ -0,0 +1,27 @@
+
+print("Calculadora")
+
+num1 = float(input("Digite um número: "))
+num2 = float(input("Digite um número: "))
+operacao = input("Digite operacação:")
+
+if operacao == "+" :
+    print(f"{num1 + num2}")
+if operacao == "-":
+    print(f"{num1 - num2}")
+if operacao == "*":
+    print(f"{num1 * num2}")
+if operacao == "/":
+    print(f"{num1 / num2}")
+
+
+
+     
+
+
+
+
+
+    
+
+    
